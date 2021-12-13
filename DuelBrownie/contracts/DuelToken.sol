7514ab2345eb63c4c64fd4fef2dc3b3a8e6a2c4f
mcruzvas <mcv.cooper@gmail.com>
2021-12-13 20:49:14 +0000

    token supply created

--- DuelBrownie/contracts/DuelToken.sol
+++ DuelBrownie/contracts/DuelToken.sol
@@ -4,6 +4,8 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract DuelToken is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_,symbol_) {
-
+        //create supply here with mint!
+        _mint(msg.sender,7000000000000000000000);
     }
+    
 }
