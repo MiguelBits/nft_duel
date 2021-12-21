85ea24e3ec622515854000a1f518f7b859bfbd00
mcruzvas <mcv.cooper@gmail.com>
2021-12-21 21:53:51 +0000

    why do I need Moralis? full web3.js login

---
+++ artifacts/abi/duelHelper_abi.js
@@ -0,0 +1,9 @@
+var duelHelper_abi = [
+    {
+        "inputs": [],
+        "name": "deposit",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    }
+];
