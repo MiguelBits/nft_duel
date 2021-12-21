61a9f37897abf4934a85db2df3bc75a029aa1db0
mcruzvas <mcv.cooper@gmail.com>
2021-12-21 01:06:03 +0000

    cleaning

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const ERC20Contract=artifacts.require('./contracts/DuelToken.sol');
-const helper=artifacts.require('./contracts/DuelHelper.sol');
-
-module.exports = function(deployer){
-    deployer.deploy(ERC20Contract);
-    deployer.deploy(helper);
-}
-
