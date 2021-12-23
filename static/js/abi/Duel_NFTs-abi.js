7db418499452e7d3eecf74f21da560dc5ca0971d
mcruzvas <mcruzvas@gmail.com>
2021-12-23 22:26:58 +0000

    change to frontend that reflect nothing

--- static/js/abi/Duel_NFTs-abi.js
+++ static/js/abi/Duel_NFTs-abi.js
@@ -1,6 +1,4 @@
-let web3 = new Web3(Web3.givenProvider);
-//CTRL + SHIFT + [ to hide variable
-var duel_nfts_abi = [
+var nfts_contract_abi = [
     {
         "inputs": [],
         "stateMutability": "nonpayable",
@@ -356,61 +354,3 @@ var duel_nfts_abi = [
         "type": "function"
     }
 ]
-const duel_nfts_Address = "0x1E5948764378ec79BF9322A3e7391Dd9102a6834"
-let nftContract = new web3.eth.Contract(duel_nfts_abi, duel_nfts_Address)
-var user;
-
-async function login() {
-  //login method
-  ethereum.request({ method: 'eth_requestAccounts' });
-  //get user to var
-  web3.eth.getAccounts().then(function(result){
-    user = web3.utils.toChecksumAddress(result[0])
-
-    //console.log(user)
-    //display and fetch balance of current user
-    balance();
-    duel_nfts_amount();
-    //console.log(user)
-  })
-  
-}
-
-async function balance(){
-  let balance;
-  web3.eth.getBalance(user, (error, balanceInWei) => {
-    balance = web3.utils.fromWei(balanceInWei, 'ether')
-    //write balance on html
-    //format to 4 decimals, change 1000 and 4 for other decimals point
-    document.getElementById("balance").innerHTML = (Math.round(balance * 10000) / 10000).toFixed(4);;
-    });
-  
-}
-
-//create/mint nft
-function open_booster_pack_Yugi(){open_booster_pack("Yugi")}
-async function open_booster_pack(_hero){
-  const nonce = await web3.eth.getTransactionCount(user, 'latest'); //get latest nonce
-
-  //the transaction
-  const tx = {
-    'from': user,
-    'to': duel_nfts_Address,
-    'nonce': nonce,
-    'gas': 5000000,
-    'maxPriorityFeePerGas': 1999999987,
-    'data': nftContract.methods.createCollectible(_hero).encodeABI()
-  };
-  const transactionReceipt = await web3.eth.sendTransaction(tx);
- 
-}
-
-//show cards next to balance
-async function duel_nfts_amount(){
-  nftContract.methods.balanceOf(user).call().then(function(result){
-    document.getElementById("cards").innerHTML = result;
-  });
-}
-
-document.getElementById("yugi").addEventListener("click", open_booster_pack_Yugi);
-document.getElementById("login-button").addEventListener("click", login);
